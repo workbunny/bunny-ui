c786b5ff500958ffaad679d0bd8b9fa57a97139a
快乐两小时 <1164601554@qq.com>
2024-12-21 11:22:51 +0800

    组件-优化：选项卡

--- doc/tab.md
+++ doc/tab.md
@@ -1,25 +1,23 @@
-<div>
-    <p>静态</p>
-    <div class="bny-tab" hx-ext="bny-tab">
-        <div class="bny-tab-title">
-            <div class="bny-tab-item">选项一</div>
-            <div class="bny-tab-item">选项二</div>
-            <div class="bny-tab-item">选项三</div>
-        </div>
-        <div class="bny-tab-body" hx-trigger="load">
-            <div>我是选项一</div>
-            <div>我是选项二</div>
-            <div>我是选项三</div>
-        </div>
+<p>静态</p>
+<div class="bny-tab" hx-ext="bny-tab">
+    <div class="bny-tab-title">
+        <div class="bny-tab-item this">选项一</div>
+        <div class="bny-tab-item">选项二</div>
+        <div class="bny-tab-item">选项三</div>
     </div>
-    <br>
-    <p>动态</p>
-    <div class="bny-tab" selected="1" hx-ext="bny-tab">
-        <div class="bny-tab-title">
-            <div class="bny-tab-item" hx-get="doc/tab/demo01.html">选项一</div>
-            <div class="bny-tab-item" hx-get="doc/tab/demo02.html">选项二</div>
-            <div class="bny-tab-item" hx-get="doc/tab/demo03.html">选项三</div>
-        </div>
-        <div class="bny-tab-body" hx-trigger="load"></div>
+    <div class="bny-tab-body">
+        <div class="this">我是选项一</div>
+        <div>我是选项二</div>
+        <div>我是选项三</div>
     </div>
+</div>
+<br>
+<p>动态</p>
+<div class="bny-tab" hx-ext="bny-tab">
+    <div class="bny-tab-title">
+        <div class="bny-tab-item" hx-get="doc/tab/demo01.html">选项一</div>
+        <div class="bny-tab-item" selected hx-get="doc/tab/demo02.html">选项二</div>
+        <div class="bny-tab-item" hx-get="doc/tab/demo03.html">选项三</div>
+    </div>
+    <div class="bny-tab-body"></div>
 </div>
--- src/bny-tab.js
+++ src/bny-tab.js
@@ -3,81 +3,95 @@ import tool from "./tool"
 
 // 拓展-选项卡
 (function () {
+    /**
+     * 初始化选项卡
+     * @param {Event} evt - 事件对象
+     */
+    function tab(evt) {
+        // 找到所有选项卡项和选项卡内容
+        const items = htmx.findAll(evt.target, ".bny-tab-item")
+        const bodys = htmx.findAll(evt.target, ".bny-tab-body>div")
 
-    function beforeProcessNode(evt) {
-        const elem = evt.target
-        if (elem !== null && evt.target.classList.contains("bny-tab")) {
-            let items = htmx.findAll(".bny-tab-item", elem)
-            items.forEach((item) => {
-                htmx.on(item, "click", function () {
-                    const index = tool.indexOf(this)
-                    const parent = this.parentNode.parentNode
-                    const body = htmx.find(parent, `.bny-tab-body>div:nth-child(${index + 1})`)
-                    htmx.findAll(parent, ".bny-tab-title .this").forEach(async function (v) {
-                        v.classList.remove("this")
-                    })
-                    htmx.findAll(parent, ".bny-tab-body .this").forEach(async function (vv) {
-                        vv.classList.remove("this")
-                    })
-                    htmx.addClass(this, "this")
-                    htmx.addClass(body, "this")
-                    if (!body.classList.contains("process")) {
-                        htmx.process(body)
-                        htmx.addClass(body, "process")
-                    }
-                })
-            });
-            return false
+        // 如果选项卡项数量多于选项卡内容数量，创建缺少的选项卡内容
+        if (items.length > bodys.length) {
+            for (let i = bodys.length; i < items.length; i++) {
+                const div = document.createElement("div");
+                htmx.find(evt.target, ".bny-tab-body").append(div);
+            }
         }
-        return true
+        // 为每个选项卡项添加点击事件
+        items.forEach((item, index) => {
+            item.onclick = () => {
+                // 获取选项卡容器
+                const tab = item.parentNode.parentNode;
+                // 找到所有选中的选项卡项和选项卡内容
+                const this_items = htmx.findAll(tab, ".bny-tab-item.this");
+                const this_bodys = htmx.findAll(tab, ".bny-tab-body>div.this");
+
+                // 移除所有选项卡项和选项卡内容的选中状态
+                this_items.forEach((i) => i.classList.remove("this"));
+                this_bodys.forEach((i) => i.classList.remove("this"));
+
+                // 设置当前选项卡项和选项卡内容为选中状态
+                item.classList.add("this");
+                htmx.find(evt.target, `.bny-tab-body>div:nth-child(${index + 1})`)
+                    .classList.add("this");
+            }
+        });
     }
 
-    function afterProcessNode(evt) {
-        const elem = evt.target
-        if (elem !== null && elem.classList.contains("bny-tab-body")) {
-            const num = htmx.findAll(elem.parentNode, ".bny-tab-title .bny-tab-item").length
-            const then_num = htmx.findAll(elem, "div").length
-            if ((num - then_num) > 0) {
-                for (let i = 0; i < (num - then_num); i++) {
-                    const div = document.createElement("div")
-                    elem.append(div)
+    /**
+     * htmx:beforeSend api
+     * @param {Event} evt 
+     */
+    function beforeSend(evt) {
+        // 获取当前选项卡项的索引
+        const index = tool.indexOf(evt.target);
+        // 找到对应的选项卡内容
+        const body = htmx.find(evt.target.parentNode.parentNode,
+            `.bny-tab-body>div:nth-child(${index + 1})`);
+        // 将请求的目标设置为对应的选项卡内容
+        evt.detail.target = body;
+    }
+
+    /**
+     * 定义一个名为 "bny-tab" 的 htmx 扩展
+     * 该扩展在特定事件发生时执行相应的操作
+     */
+    htmx.defineExtension("bny-tab", {
+        /**
+         * 事件处理函数
+         * @param {string} name - 事件名称
+         * @param {Event} evt - 事件对象
+         */
+        onEvent: function (name, evt) {
+            // 当 htmx 处理完一个节点后
+            if (name === "htmx:afterProcessNode") {
+                // 如果事件目标包含 "bny-tab" 类
+                if (evt.target.classList.contains("bny-tab")) {
+                    // 调用 tab 函数初始化选项卡
+                    tab(evt);
+                }
+                // 如果事件目标包含 "bny-tab-item" 类
+                if (evt.target.classList.contains("bny-tab-item")) {
+                    // 获取 "selected" 属性的值
+                    const selected = evt.target.getAttribute("selected");
+                    // 如果 "selected" 属性存在
+                    if (selected !== null) {
+                        // 模拟点击事件
+                        evt.target.click();
+                    }
+                    // 阻止事件冒泡
+                    return false;
                 }
             }
-            let parent = elem.parentNode
-            const selected = parent.getAttribute("selected") ?? 0
-            const default_item = htmx.find(parent,
-                `.bny-tab-title .bny-tab-item:nth-child(${Number(selected) + 1})`)
-            default_item.click()
-        }
-    }
-
-    function beforeSwap(evt) {
-        const elem = evt.target
-        if (elem !== null && elem.classList.contains("bny-tab-item")) {
-            const index = tool.indexOf(elem)
-            const body = htmx.find(elem.parentNode.parentNode, `.bny-tab-body>div:nth-child(${index + 1})`)
-            if (evt.detail.xhr.status === 200) {
-                htmx.swap(body, evt.detail.serverResponse, { swapStyle: "innerHTML" })
-                // body.innerHTML = evt.detail.serverResponse
-            } else {
-                htmx.swap(body, "网络错误...", { swapStyle: "innerHTML" })
-            }
-        }
-    }
-
-    htmx.defineExtension("bny-tab", {
-        onEvent: function (name, evt) {
-            // console.log(name, evt)
-            if (name === "htmx:beforeProcessNode") {
-                return beforeProcessNode(evt)
-            }
-            if (name === "htmx:afterProcessNode") {
-                afterProcessNode(evt)
-                return false
-            }
-            if (name === "htmx:beforeSwap") {
-                beforeSwap(evt)
-                return false
+            // 在发送请求之前
+            if (name === "htmx:beforeSend") {
+                // 如果事件目标包含 "bny-tab-item" 类
+                if (evt.target.classList.contains("bny-tab-item")) {
+                    // 调用 beforeSend 函数
+                    beforeSend(evt);
+                }
             }
         }
     })
