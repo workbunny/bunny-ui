d2ba8ef8b110542fd4b1494570b26a43597cab35
快乐两小时 <1164601554@qq.com>
2024-12-23 15:23:39 +0800

    优化组件：弹出层

--- src/bunny.js
+++ src/bunny.js
@@ -16,14 +16,17 @@ class bunny {
      */
     msg({ text = "消息", style = "bg-white", fn = () => { }, time = 2000 }) {
         const layer = document.createElement("div")
-        layer.className = "bny-layer"
+        layer.className = "bny-layer bny-anim-scale"
         layer.innerHTML =
             `<div class="bny-msg ${style}">
             ${text}
         </div>`
         document.body.appendChild(layer)
         setTimeout(() => {
-            layer.remove()
+            layer.classList.add("bny-anim-scalesmall")
+            setTimeout(function () {
+                layer.remove()
+            }, 230)
             fn(layer)
         }, time)
     }
@@ -43,7 +46,7 @@ class bunny {
 
         // 设置样式类
         shield.className = "bny-shield"
-        confirmBox.className = "bny-confirm"
+        confirmBox.className = "bny-confirm bny-anim-scale"
 
         // 创建确认框的HTML结构
         confirmBox.innerHTML = `
@@ -58,14 +61,20 @@ class bunny {
 
         // 添加点击事件到遮罩层（关闭模态）
         shield.addEventListener("click", function handleClick(event) {
-            shield.remove();
+            confirmBox.classList.add("bny-anim-scalesmall")
+            setTimeout(function () {
+                shield.remove();
+            }, 230)
         });
 
         // 阻止点击确认框时冒泡到遮罩层
         confirmBox.addEventListener("click", event => {
             if ((event.target.closest('.border-muted') && cancel()) ||
                 (event.target.closest('.border-warning') && yes())) {
-                shield.remove()
+                confirmBox.classList.add("bny-anim-scalesmall")
+                setTimeout(function () {
+                    shield.remove();
+                }, 230)
             }
             event.stopPropagation()
         });
