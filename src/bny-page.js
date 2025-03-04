75beed4eb13ea8df86e8c89a3b89a674989ac05f
快乐两小时 <1164601554@qq.com>
2025-03-04 10:59:29 +0800

    优化组件

--- src/bny-page.js
+++ src/bny-page.js
@@ -8,15 +8,18 @@ import htmx from "./htmx"
         const width = target.getAttribute("bny-width") || "680px"
         const height = target.getAttribute("bny-height") || "520px"
         const offset = target.getAttribute("bny-offset") || "auto"
-        const shade = target.getAttribute("bny-shade") || false
+        let shade = target.getAttribute("bny-shade") || false
         const anim = target.getAttribute("bny-anim") || 0
+        if (shade === "true") {
+            shade = true
+        }
         bunny.page({
             title: title,
             width: width,
             height: height,
             offset: offset,
             shade: shade,
-            anim: anim,
+            anim: parseInt(anim),
             content: evt.detail.xhr.response
         })
         return false;
