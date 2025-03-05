cd3fd1f4cf6863cf4971e58382981dfcef712e0a
快乐两小时 <1164601554@qq.com>
2025-03-05 17:09:09 +0800

    优化弹出框

--- src/bny-page.js
+++ src/bny-page.js
@@ -7,17 +7,18 @@ import htmx from "./htmx"
         const title = target.getAttribute("bny-title") || false
         const width = target.getAttribute("bny-width") || "680px"
         const height = target.getAttribute("bny-height") || "520px"
-        const offset = target.getAttribute("bny-offset") || "auto"
+        const offset = (target.getAttribute("bny-offset") || "auto").split(",")
         let shade = target.getAttribute("bny-shade") || false
         const anim = target.getAttribute("bny-anim") || 0
         if (shade === "true") {
             shade = true
         }
+
         bunny.page({
             title: title,
             width: width,
             height: height,
-            offset: offset,
+            offset: offset.length == 2 ? offset : offset[0],
             shade: shade,
             anim: parseInt(anim),
             content: evt.detail.xhr.response
